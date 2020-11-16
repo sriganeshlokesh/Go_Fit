ed33a5a102e2541bdf3914890aebbdabd55cf175
Sriganesh Lokesh <sriganeshlokesh@Sriganeshs-MacBook-Pro.local>
2020-11-15 22:11:57 -0500

    Changes to Server.js

--- server.js
+++ server.js
@@ -48,6 +48,15 @@ app.use("/api/blog", blog);
 app.use("/api/appointment", appointment);
 app.use("/api/user", user);
 
+// Server Static Assets in Production
+if (process.env.NODE_ENV === "production") {
+  // Set Static Folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // Static Middleware
 app.use(express.static("public"));
 
